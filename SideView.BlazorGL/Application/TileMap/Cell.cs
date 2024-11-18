25cfdaa5ce051dfe23691e9d205af2e2e55e6e0f
Sven Hendriks <100040414+shendriks@users.noreply.github.com>
2024-11-18 21:35:35 +0100

    Implement GridNavigator

--- SideView.BlazorGL/Application/TileMap/Cell.cs
+++ SideView.BlazorGL/Application/TileMap/Cell.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 
 namespace Pathfinding2D.SideView.BlazorGL.Application.TileMap;
 
-public class Cell(int x, int y, CellType type, Grid grid)
+public class Cell(int x, int y, CellType type)
 {
     private CellType _type = type;
 
@@ -31,15 +31,6 @@ public class Cell(int x, int y, CellType type, Grid grid)
     public bool IsInOpenSet { get; set; }
     public bool WasInspected { get; set; }
     public bool IsCurrentlyBeingExamined { get; set; }
-    public Cell? Up => NeighborAt(0, -1);
-    public Cell? Down => NeighborAt(0, 1);
-    public Cell? Left => NeighborAt(-1, 0);
-    public Cell? Right => NeighborAt(1, 0);
-    public Cell? DownRight => NeighborAt(1, 1);
-    public Cell? DownLeft => NeighborAt(-1, 1);
-    public Cell? UpRight => NeighborAt(1, -1);
-    public Cell? UpLeft => NeighborAt(-1, -1);
-    public Cell? NeighborAt(int deltaX, int deltaY) => grid[X + deltaX, Y + deltaY];
 
     public event Action? CellTypeChanged;
 
